d0f204bbc27cf34565468321cdc3aa3f18f5a443
daedboi <87483308+daedboi@users.noreply.github.com>
2021-11-08 14:50:29 +0200

    Fix eth to ftm and use new zapper

--- src/pages/Migrate/index.tsx
+++ src/pages/Migrate/index.tsx
@@ -127,7 +127,11 @@ const Migrate = () => {
   }, [account])
 
 
+<<<<<<< Updated upstream
   const [approval, approveCallback] = useApproveCallback(tryParseAmount('9999999999', token), chainId && '0xD5b6BD4AC0F4a106588B5658Bf322bBF64add88e')
+=======
+  const [approval, approveCallback] = useApproveCallback(tryParseAmount('9999999999', token), chainId && '0xDD9Ac0d6B5DBD3b009acc36ba40B4db657881e11')
+>>>>>>> Stashed changes
   console.log('the approval is', approval)
   const [inputAmount, setInputAmount] = useState('');
   const [inputFloatAmount, setInputFloatAmount] = useState(0);
@@ -283,7 +287,7 @@ const Migrate = () => {
   const onMigrate = async e => {
     console.log('on migrate');
     if(!library) return;
-    const zapContract = getContract('0xD5b6BD4AC0F4a106588B5658Bf322bBF64add88e', zapABI, library);
+    const zapContract = getContract('0xDD9Ac0d6B5DBD3b009acc36ba40B4db657881e11', zapABI, library);
     console.log('the zap contract is',zapContract);
     if(!token || !inputAmount || !library) return;
 
