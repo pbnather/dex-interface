dc8313e8e4bb8d42a6f118f865feba5b27f05ad3
daedboi <87483308+daedboi@users.noreply.github.com>
2021-11-09 15:53:21 +0200

    Try diff multicall
    Old: 0x263729a73F5566638Cc08f6ABBd6a81dcF10626A

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from 'morph-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x263729a73F5566638Cc08f6ABBd6a81dcF10626A', // TODO
+  [ChainId.MAINNET]: '0xeDa5a4a383CA7584EfbB7f8d5C06e6440D9c0B66', // TODO
   [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
 }
 
